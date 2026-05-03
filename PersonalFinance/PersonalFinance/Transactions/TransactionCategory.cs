f4808828b43db2a3ade6bdda11fe6594d35124d2
TheAdvental <tvlordveider@gmail.com>
2026-05-03 16:24:58 +0300

    Started creating categories/subcategories

--- PersonalFinance/PersonalFinance/Transactions/TransactionCategory.cs
+++ PersonalFinance/PersonalFinance/Transactions/TransactionCategory.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 namespace PersonalFinance.Transactions
 {
-    public enum TransactionCategory
+    public class TransactionCategory
     {
-        Groceries,
-        Entertainment,
-        Sports,
-        Travels,
-        Charity,
-        Debts
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TransactionCategory ParentCategory { get; set; }
+        private List<TransactionCategory> _subCategories = new List<TransactionCategory>();
+        public IReadOnlyList<TransactionCategory> SubCategories => _subCategories;
+        //Groceries,
+        //Entertainment,
+        //Sports,
+        //Travels,
+        //Charity,
+        //Debts
+        public TransactionCategory(string name)
+        {
+            Name = name;
+        }
+
+        public TransactionCategory(string name, TransactionCategory parent) : this(name)
+        {
+            ParentCategory = parent;
+            parent._subCategories.Add(this);
+        }
     }
 }
